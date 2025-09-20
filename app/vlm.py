2db1d800e7ef25665561a2d041c81ace3121898c
aaronshin43 <aaronshin.exe@gmail.com>
2025-09-20 03:22:44 -0400

    change ai model

--- app/vlm.py
+++ app/vlm.py
@@ -10,7 +10,7 @@ if not API_KEY:
     raise ValueError("GEMINI_API_KEY not found in .env file")
 
 genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash-lite')
 # -------------
 
 def generate_prompt(word_to_guess: str, choices: list) -> str:
